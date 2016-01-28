03696cdc3d60f1bc310635aa40eaa1f2e4e9726d
msjche <msjche@gmail.com>
2016-01-28 15:50:40 -0800

    mirrorlist script

--- Scripts/mirrors.sh
+++ Scripts/mirrors.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup &
-
-sleep 2
-
-/usr/bin/reflector -c 'United States' -f 12 -l 12 --verbose --save /etc/pacman.d/mirrorlist &
-
-/n
-
-echo "Mirrorlist Updated !!"
+/usr/bin/reflector -c 'United States' -f 12 -l 12 --verbose --save /etc/pacman.d/mirrorlist
